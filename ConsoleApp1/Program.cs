e4bdfa46a9c601dfe638d32049af9586d9acd63c
pdesgn <pdesgn@DESKTOP-LC4MO2N>
2021-02-15 00:38:55 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//typesafety
+			string kategorietiketi = "kategori";
+			int ogrencisayisi = 32000;
+			bool sistemegirisyapmismi = true;
+
+			double dolarDun = 7.35;
+			double dolarBugun = 7.45;
+
+			if (dolarDun>dolarBugun)
+			{
+				Console.WriteLine("Azalış Butonu");
+			}
+			else if (dolarDun<dolarBugun)
+			{
+				Console.WriteLine("Artış oku");
+			}
+			else	
+			{
+				Console.WriteLine("Giris yap butonu");
+			}
+
+			if (sistemegirisyapmismi == true)
+			{
+				Console.WriteLine("Kulanıcı ayarları butonu");
+						
+			}
+			else
+			{
+				Console.WriteLine("Giris yap butonu");
+			}
+			Console.WriteLine(kategorietiketi);
+
+
+		}
+	}
+}
---
+++ donguler/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace donguler
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string kurs1 = "Yazilim Geliştrici Yetiştirme kampı";
+			string kurs2 = "Programlamaya başlamak için temel kurs";
+			string kurs3 = "Java";
+
+			//array = dizi
+
+			string[] kurslar = new string[] { "Yazilim Geliştrici Yetiştirme kampı", 
+				"Programlamaya başlamak için temel kurs", "Java" };
+
+			for (int i = 0; i < kurslar.Length; i++)
+			{
+				Console.WriteLine(kurslar[i]);
+			}
+
+            foreach (string kurs in kurslar)
+            {
+				Console.WriteLine(kurs);
+            }
+
+
+			Console.WriteLine("Console sonu");
+		
+		}
+	}
+}
